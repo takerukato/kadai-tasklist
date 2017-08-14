6ddcf523c2073a5b1c5f9d188e811b06cb5b0918
TakeruKato <bkbc1031@gmail.com>
2017-08-14 10:14:32 +0000

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = task.new(task_params)
+    @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = 'Task　が正常に投稿されました'
